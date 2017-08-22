98a7da3bf522c4a7ee4064fc28562376e49f4693
Ary Medeiros <medeiros.ary@uol.com.br>
2017-08-22 18:59:10 -0300

    Initial Commit

---
+++ views/demo/framework/index.php
@@ -0,0 +1,27 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<div class="ft-container ft-col-100">
+
+	<h1>Flytech Framework</h1>
+	
+    <div class="ft-col-100">
+      <h3>Principais características:</h3>
+      
+      <div class="ft-col-100"><i class="fa fa-check"></i> Multitenancy Nativo X</div>
+      <div class="ft-col-100"><i class="fa fa-check"></i> Gerenciamento de Usuários e Perfil de Acesso</div>
+      <div class="ft-col-100"><i class="fa fa-check"></i> Modelo MVC de fácil aprendizado</div>
+      <div class="ft-col-100"><i class="fa fa-check"></i> Infraestrutura com Administração Zero</div>
+      <div class="ft-col-100"><i class="fa fa-check"></i> Modelos altamente customizáveis</div>
+      <div class="ft-col-100"><i class="fa fa-check"></i> Integração Nativa com o ERPFlex</div>
+      <div class="ft-col-100"><i class="fa fa-check"></i> Ferramentas de Produtividade:</div>
+	  <div class="ft-col-100">
+		<ul class="tools">
+			<li>Gerador de Relatórios</li>
+			<li>Gerador de Consultas</li>
+			<li>Gráficos</li>
+			<li>Pivot</li>
+		</ul> 
+  	  </div>
+  </div>
+</div>
